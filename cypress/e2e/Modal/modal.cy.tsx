8674fa294362dc691667a787861609296b81f6d8
Olga <o-lin98@mail.ru>
2025-06-10 15:58:56 +0300

    Написаны все тесты cypress

--- cypress/e2e/Modal/modal.cy.tsx
+++ cypress/e2e/Modal/modal.cy.tsx
@@ -1,55 +1,17 @@
-const baseUrl = 'http://localhost:4000';
-const modal = '[data-cy="modal"]';
-const modalCloseButton = '[data-cy="modal-close-button"]';
-const bunName = 'Флюоресцентная булка R2-D3';
-
-const ingredients = '[data-cy="ingedients-in-constructor"]';
-
-beforeEach(() => {
-  // Перехватываем GET-запрос к API ингредиентов и подменяем ответ данными из json
-  cy.intercept('GET', 'api/ingredients', {
-    fixture: 'ingredients.json'
-  }).as('getIngredients');
-
-  cy.visit(baseUrl);
-
-  // Ждем пока не будет выполнен перехваченный запрос
-  cy.wait('@getIngredients');
-});
-
-describe('Тест конструктора', () => {
-  it('Тест добавление ингридиентов', () => {
-    // Добавляем булку
-    cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093d"] button').click();
-
-    // Добавляем начинку
-    cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0941"] button').click();
-
-    // Добавляем соус
-    cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa0942"] button').click();
-
-    // Проверяем, что верхняя часть добавлена
-    cy.get('[data-cy="constructor-bun-top"]').should(
-      'contain',
-      'Флюоресцентная булка R2-D3'
-    );
-
-    // Проверяем, что начинка добавлена
-    cy.get(ingredients).should('contain', 'Биокотлета из марсианской Магнолии');
-
-    // Проверяем, что соус добавлен
-    cy.get(ingredients).should('contain', 'Соус Spicy-X');
-
-    // Проверяем, что нижняя часть добавлена
-    cy.get('[data-cy="constructor-bun-bottom"]').should(
-      'contain',
-      'Флюоресцентная булка R2-D3'
-    );
-  });
-});
+import { baseUrl, modal, modalCloseButton, bunName } from './constuctor.cy';
 
 describe('Тест работы модального окна', () => {
   beforeEach(() => {
+    // Перехватываем GET-запрос к API ингредиентов и подменяем ответ данными из json
+    cy.intercept('GET', 'api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+
+    cy.visit(baseUrl);
+
+    // Ждем пока не будет выполнен перехваченный запрос
+    cy.wait('@getIngredients');
+
     // Кликаем на булку, чтобы открыть модальное окно
     cy.get('[data-cy="ingredient-643d69a5c3f7b9001cfa093d"]').click();
   });
@@ -64,7 +26,7 @@ describe('Тест работы модального окна', () => {
   it('Тест закрытия модального окна', () => {
     cy.get(modalCloseButton).click();
 
-    // Проверяем, что модальное окно больше не существует на странице
+    // Проверяем, что модальное окно больше не существует
     cy.get(modal).should('not.exist');
   });
 
